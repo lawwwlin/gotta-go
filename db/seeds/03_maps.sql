055b5b8372769cc687afcc4bf92613a64254bb22
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-24 16:05:40 -0700

    fix point data type in sample data

--- db/seeds/03_maps.sql
+++ db/seeds/03_maps.sql
@@ -1,3 +1,3 @@
 INSERT INTO maps (creator_id, name, location)
-VALUES ('vancouver_boy123', 'Vancouver Downtown', (49.2827, 123.1207)),
+VALUES ('vancouver_boy123', 'Vancouver Downtown', point(49.2827, 123.1207)),
 ('van_island_lover', 'Victoria', point(49.6506, 125.4494));
