034ec616f70cfd7381a92d1e2d7b55c699245090
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-24 16:00:30 -0700

    Update 01_users.sql

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -4,5 +4,5 @@ ALTER COLUMN password
 SET DEFAULT '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.';
 
 INSERT INTO users (username, location)
-VALUES ('vancouver_boy123', (49.2827, 123.1207)),
-VALUES ('victoria_secret', (48.42850412297416, -123.36566147114377));
+VALUES ('vancouver_boy123', point(49.2827, 123.1207)),
+('victoria_secret', point(48.42850412297416, -123.36566147114377));
