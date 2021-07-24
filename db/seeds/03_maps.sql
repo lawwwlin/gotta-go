edf45b01d2e183e3b21396ac575c7f7a511c6816
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-24 16:00:34 -0700

    Update 03_maps.sql

--- db/seeds/03_maps.sql
+++ db/seeds/03_maps.sql
@@ -1,3 +1,3 @@
 INSERT INTO maps (creator_id, name, location)
 VALUES ('vancouver_boy123', 'Vancouver Downtown', (49.2827, 123.1207)),
-VALUES ('van_island_lover', 'Victoria', (49.6506, 125.4494));
+('van_island_lover', 'Victoria', point(49.6506, 125.4494));
