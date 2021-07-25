56b7e4e06cb769de40f673c24cae1d5c30b905c9
Sylas Serné <its.sylas@gmail.com>
2021-07-25 17:17:40 +0000

    initialize map set up and website

---
+++ public/scripts/leaflet.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+const map = L.map('map', {
+  center: [48.42959706075791, -123.34509764072138],
+  zoom: 13
+});
+
+L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=IWRRuvOlBlyhZTVNm8VO', {
+  attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+}).addTo(map);
+
+});
