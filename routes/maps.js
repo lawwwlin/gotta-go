d0051bd52aee80f1697c3f2361c921e453b11dac
Catherine Hu <catherinehu_9898@hotmail.com>
2021-07-25 06:38:51 +0000

    added edit map

--- routes/maps.js
+++ routes/maps.js
@@ -12,7 +12,7 @@ module.exports = (db) => {
   //retrieves all maps in database
   router.get("/", (req, res) => {
     db.query(`SELECT * FROM maps;`)
-    .then(data => {
+      .then(data => {
         const maps = data.rows;
         res.json({ maps });
       })
@@ -27,34 +27,50 @@ module.exports = (db) => {
   router.get("/:id", (req, res) => {
     const values = req.params.id;
     db.query(`SELECT * FROM maps WHERE maps.id = $1`, [values])
-    .then (data => {
-      const maps = data.rows;
-      res.json({ maps }) // change to res.render("maps") later on
-    })
-    .catch(err => {
-      res.status(500)
-      .json({ error: err.message })
-    })
+      .then(data => {
+        const maps = data.rows;
+        res.json({ maps }) // change to res.render("maps") later on
+      })
+      .catch(err => {
+        res.status(500)
+          .json({ error: err.message })
+      })
   })
 
-    //get favourited maps
-    router.get("/:id/favourited_maps", (req, res) => {
-      const values = req.params.id;
-      db.query(`SELECT * FROM favourited_maps
+  //get favourited maps
+  router.get("/:id/favourited_maps", (req, res) => {
+    const values = req.params.id;
+    db.query(`SELECT * FROM favourited_maps
       JOIN maps ON maps.id = map_id
       JOIN users ON users.id = users.id
       WHERE user_id = $1;`, [values])
-        .then(data => {
-          const favorites = data.rows;
-          res.json({ favorites }); // change to res.render("favourite_maps") later on
-        })
-        .catch(err => {
-          res
-            .status(500)
-            .json({ error: err.message });
-        });
-    });
+      .then(data => {
+        const favorites = data.rows;
+        res.json({ favorites }); // change to res.render("favourite_maps") later on
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
 
+  //edit map
+  router.patch("/:id", (req, res) => {
+    const values = req.params.id;
+    const { name, area, creator_id } = req.body;
+    const query = `UPDATE maps SET name = $1, area = $2 WHERE creator_id = $3 AND maps_id = $4 RETURNING *;`;
+    db.query(query, [name, area, creator_id, values])
+      .then(data => {
+        const maps = data.rows;
+        res.json({ maps });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
 
   //delete map
   router.delete("/:id", (req, res) => {
