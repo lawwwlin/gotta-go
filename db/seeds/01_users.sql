3ed31a52e9c1b1fcaa87166b6eeb93c61edc51d2
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-07-24 19:23:56 -0700

    remove location column

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -3,6 +3,6 @@ ALTER TABLE users
 ALTER COLUMN password
 SET DEFAULT '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.';
 
-INSERT INTO users (username, location, latitude, longitude)
+INSERT INTO users (username, latitude, longitude)
 VALUES ('vancouver_boy123', 49.2827, 123.1207),
 ('victoria_secret', 48.42850412297416, -123.36566147114377);
