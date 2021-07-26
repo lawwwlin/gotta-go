12105c4df0b3bc2ffa6a4583fc298d86b5eb9a67
Sylas Serné <its.sylas@gmail.com>
2021-07-26 18:13:02 +0000

    init commit

--- public/scripts/pins.js
+++ public/scripts/pins.js
@@ -1,1 +1,10 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'db'
+});
+
 const pins.id = L.marker([pins.location], draggable = false, title = pins.title)
