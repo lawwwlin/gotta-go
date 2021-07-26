bf9962511509513b4af9845439cb232f6c8dc7df
Sylas Serné <its.sylas@gmail.com>
2021-07-26 15:22:34 +0000

    resize the map

--- public/scripts/leaflet.js
+++ public/scripts/leaflet.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
 const map = L.map('map', {
-  center: [48.42959706075791, -123.34509764072138],
+  center: [48.42959706075791, -123.34509764072138], //set this to user location
   zoom: 13
 });
 
 L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=IWRRuvOlBlyhZTVNm8VO', {
   attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
 }).addTo(map);
+
+L.marker([48.43037425991212, -123.34502630954228], draggable = false, tile = 'Little June Cafe').addTo(map)
 
 });
