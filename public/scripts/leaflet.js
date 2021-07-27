c1bd95be732208d39cd473d985ac95a046b4c73a
Sylas Serné <its.sylas@gmail.com>
2021-07-27 16:56:26 +0000

    implement loading pins within a certain radius to decrease lag

--- public/scripts/leaflet.js
+++ public/scripts/leaflet.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     zoom: 13
   })
 
-  console.log('map.center:', map.getCenter())
+  console.log('map.center:', typeof(map.getCenter().lat))
 
   L.tileLayer('https://api.maptiler.com/maps/pastel/{z}/{x}/{y}.png?key=IWRRuvOlBlyhZTVNm8VO', {
     attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
@@ -14,6 +14,19 @@ $(document).ready(function() {
 function makePin(pin) {
   return L.marker([pin.latitude, pin.longitude], title = pin.title).addTo(map)
 }
+
+
+const mapLat = map.getCenter().lat
+const mapLng = map.getCenter().lng
+
+function radiusCheck(pin, rad) {
+  if (mapLat - rad <= pin.latitude && pin.latitude <= mapLat + rad) {
+    if (mapLng - rad <= pin.longitude && pin.longitude <= mapLng + rad){
+      return true;
+    }
+  }
+}
+
 //show all pins
 // $.get('/api/pins', (obj) => {
 //   for (const pin of obj.pins) {
@@ -21,13 +34,18 @@ function makePin(pin) {
 //   }
 // })
 
-// $.get('/api/pins', (obj) => {
-//   for (const pin of obj.pins) {
-//     if (pin.latitude >= map.getCenter().lat +- 0.6 && pin.longitude >= pin.longitude + 0.6) {
-//       makePin(pin);
-//     }
-//   }
-// })
+//map.on('drag', {
+  $.get('/api/pins', (obj) => {
+    for (const pin of obj.pins) {
+      //if the pin is in a ~100km radius of the center of the map then it will load to the map
+      if (radiusCheck(pin, 0.6)) {
+        makePin(pin);
+      } else {
+        console.log('no pin in range')
+      }
+    }
+  })
+//})
 
 // pin.on('click', alert("wooooah"));
 console.log(arrOfPins);
